2cb66dd186e7e991fb4c702cdc510fe5ae88e322
Filip Melka <melka.filip@proton.me>
2025-10-30 22:10:04 +0100

    feat: add gallery placeholder

--- packages/client/app/display-files.tsx
+++ packages/client/app/display-files.tsx
@@ -1,12 +1,17 @@
 import { useClientContext } from '@/context/client-context'
 import { Gallery } from './gallery'
+import { GalleryPlaceholder } from './gallery-placeholder'
 
 export function DisplayFiles() {
   const { sharedFiles } = useClientContext()
 
   return (
     <div className="mb-20 mt-5 xl:px-[10%]">
-      <Gallery files={sharedFiles} />
+      {sharedFiles.length > 0 ? (
+        <Gallery files={sharedFiles} />
+      ) : (
+        <GalleryPlaceholder />
+      )}
     </div>
   )
 }
---
+++ packages/client/app/gallery-placeholder.tsx
@@ -0,0 +1,23 @@
+import { Image as ImageIcon } from 'lucide-react'
+
+export function GalleryPlaceholder() {
+  return (
+    <div className="flex min-h-[calc(100dvh-20rem)] flex-col items-center justify-evenly">
+      <ImageIcon size={250} className="opacity-20" />
+      <p className="text-center text-lg opacity-90">
+        Click on the <Btn /> button and select photos you want to transfer.
+      </p>
+    </div>
+  )
+}
+
+function Btn() {
+  return (
+    <div className="inline-block px-2">
+      <span className="flex items-center gap-1 rounded-lg bg-black px-3 py-1.5 text-base text-white">
+        <ImageIcon size={20} />
+        <span>Choose Photos</span>
+      </span>
+    </div>
+  )
+}
