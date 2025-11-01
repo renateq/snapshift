1a10d0a47c42fdd8a72b5a09cfa8e98f0d1257b5
Filip Melka <melka.filip@proton.me>
2025-11-01 13:07:19 +0100

    feat: add download all button

--- packages/client/app/gallery.tsx
+++ packages/client/app/gallery.tsx
@@ -1,3 +1,4 @@
+import { DownloadAllBtn } from './download-all-btn'
 import { FilePreview } from './file-preview'
 
 type GalleryProps = {
@@ -6,16 +7,21 @@ type GalleryProps = {
 
 export function Gallery({ files }: GalleryProps) {
   return (
-    <div
-      id="gallery"
-      className="flex w-full flex-wrap items-start justify-center gap-x-0 gap-y-12 py-8"
-    >
-      {files.map((file) => (
-        <FilePreview
-          file={file}
-          key={`${file.name}-${file.lastModified}-${file.size}`}
-        />
-      ))}
+    <div className="py-4">
+      <div className="flex justify-end pb-4">
+        <DownloadAllBtn files={files} />
+      </div>
+      <div
+        id="gallery"
+        className="flex w-full flex-wrap items-start justify-center gap-x-0 gap-y-12 py-8"
+      >
+        {files.map((file) => (
+          <FilePreview
+            file={file}
+            key={`${file.name}-${file.lastModified}-${file.size}`}
+          />
+        ))}
+      </div>
     </div>
   )
 }
